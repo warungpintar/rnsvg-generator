d5f3a2967770bfdf11e56f6b458f9436398532cd
adeyahya <adeyahyaprasetyo@gmail.com>
2021-07-26 13:34:47 +0700

    fix: async operation

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,6 @@ import { Command } from "commander";
 import { readdir } from "fs/promises";
 import ora from "ora";
 
-function getFile(file: string) {
-  return path.resolve(process.cwd(), file);
-}
-
 async function getFiles(dir: string): Promise<string[]> {
   const dirents = await readdir(dir, { withFileTypes: true });
   const files: any[] | Promise<any> = await Promise.all(
@@ -26,39 +22,36 @@ async function getFiles(dir: string): Promise<string[]> {
     .filter((file) => path.extname(file) === ".svg");
 }
 
-const parseToJSXComponent = (
-  filePath: string,
-  outputPath: string,
-  callback?: () => any
-) => {
-  if (!filePath) {
-    process.exit(0);
-  }
-
+const parseToJSXComponent = (filePath: string, outputPath: string) => {
   const componentName = _upperFirst(
     _camelCase(path.basename(filePath, ".svg"))
   );
 
-  parser(filePath, componentName).then((text) => {
-    fs.mkdirSync(outputPath, { recursive: true });
-    fs.writeFile(
-      path.join(outputPath, componentName + ".tsx"),
-      text,
-      {
-        encoding: "utf-8",
-      },
-      (error) => {
-        if (error) {
-          console.warn(`error converting: ${filePath}`);
-        } else {
-          if (callback) {
-            callback();
-          } else {
-            process.exit(0);
-          }
-        }
+  return new Promise((resolve) => {
+    parser(filePath, componentName).then((text) => {
+      const isHasExtension = outputPath.match(/\.+[a-zA-Z]+$/);
+
+      if (!isHasExtension) {
+        fs.mkdirSync(outputPath, { recursive: true });
       }
-    );
+
+      fs.writeFile(
+        isHasExtension
+          ? outputPath
+          : path.join(outputPath, componentName + ".tsx"),
+        text,
+        {
+          encoding: "utf-8",
+        },
+        (error) => {
+          if (error) {
+            console.warn(`error converting: ${filePath}`);
+          }
+
+          resolve(null);
+        }
+      );
+    });
   });
 };
 
@@ -67,30 +60,36 @@ program.argument("<sourceDir>").requiredOption("-o --output <value>");
 
 program.action(async (sourcePath: string, { output }) => {
   const spinner = ora("generating components").start();
-  const stats = fs.lstatSync(sourcePath);
   const cwd = process.cwd();
   const outputPath = output.match(/^(\~|\/)/) ? output : path.join(cwd, output);
   sourcePath = sourcePath.match(/^(\~|\/)/)
     ? sourcePath
     : path.join(cwd, sourcePath);
+  const stats = fs.lstatSync(sourcePath);
 
   if (stats.isFile()) {
-    const filePath = getFile(sourcePath);
-    parseToJSXComponent(filePath, outputPath);
+    await parseToJSXComponent(sourcePath, outputPath);
   }
 
   if (stats.isDirectory()) {
     const files = await getFiles(sourcePath);
     const filesIterator = files[Symbol.iterator]();
 
-    const iterate = () => {
+    const iterate = async () => {
       const filePath = filesIterator.next().value;
-      parseToJSXComponent(filePath, outputPath, iterate);
-      spinner.text = `${filePath} converted successfully`;
+
+      if (filePath) {
+        await parseToJSXComponent(filePath, outputPath);
+        spinner.text = `${path.basename(filePath)} converted successfully`;
+        await iterate();
+      }
     };
 
-    iterate();
+    await iterate();
   }
+
+  spinner.stop();
+  process.exit(0);
 });
 
 program.parse(process.argv);
