2d70db7ce470b453255ae04cbb0a21b9f921a2e5
adeyahya <adeyahyaprasetyo@gmail.com>
2021-07-27 09:12:17 +0700

    color memoizer

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,12 @@ import { PathLike } from "fs";
 import fs from "fs";
 
 const IGNORED_UNITS_RE = /px|pt|rem|em|\%/;
+const NIL_COLORS_IN_CSS = ["none"];
+
+type MemoizedColors = {
+  outer?: string;
+  inner?: string;
+};
 
 export const readdir = (path: PathLike) => {
   try {
@@ -22,6 +28,38 @@ export const getSvgFiles = (sourcePath: string): string[] => {
   return Array.prototype
     .concat(...files)
     .filter((file) => path.extname(file) === ".svg");
+};
+
+/**
+ * create memoized function for
+ * color assignment, inner / outer / not assigned to both
+ * @returns
+ */
+export const createColorMemoizer = () => {
+  const colors: MemoizedColors = { inner: undefined, outer: undefined };
+  return (color: string) => {
+    const result = { outer: false, inner: false };
+
+    if (
+      (!NIL_COLORS_IN_CSS.includes(color) && colors.outer === undefined) ||
+      colors.outer === color
+    ) {
+      colors.outer = color;
+      result.outer = true;
+      return result;
+    }
+
+    if (
+      (!NIL_COLORS_IN_CSS.includes(color) && colors.inner === undefined) ||
+      colors.inner === color
+    ) {
+      colors.inner = color;
+      result.inner = true;
+      return result;
+    }
+
+    return result;
+  };
 };
 
 /**
